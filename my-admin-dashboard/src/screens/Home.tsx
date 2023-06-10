d01233268938b1a951b5b1ac38ebb69782775baf
Lexpiper <lexpiperfx@gmail.com>
2023-06-10 14:21:19 +0100

    Implement responsive layout and fix sidebar overlap issue in Home component

--- my-admin-dashboard/src/screens/Home.tsx
+++ my-admin-dashboard/src/screens/Home.tsx
@@ -1,15 +1,19 @@
-import Navbar from "../components/Navbar/Navbar"
-import Sidebar from "../components/sidebar/Sidebar"
+import Navbar from "../components/Navbar/Navbar";
+import Widgets from "../components/Widgets/Widgets";
+import Sidebar from "../components/sidebar/Sidebar";
 
 const Home = () => {
   return (
     <div className="flex flex-6">
-        <Sidebar/>
-        <div className="flex-1">
-            <Navbar/>
+      <Sidebar />
+      <div className="ml-64 flex-1 bg-[#f5f4f7] h-screen">
+        <Navbar />
+        <div className="widgets flex md:flex-row flex-col gap-[20px] py-[5px] px-[10px] mt-5 shadow-sm  ">
+          <Widgets />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
