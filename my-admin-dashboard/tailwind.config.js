05703ec3c9ecf11f95ce136e11400f59bd903f8d
Lexpiper <lexpiperfx@gmail.com>
2023-06-13 15:42:06 +0100

    added font extention

--- my-admin-dashboard/tailwind.config.js
+++ my-admin-dashboard/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        Sfd: ["'SF Pro Display', sans-serif"],
+        Sfd: ["SF Pro Display, sans-serif"],
       },
     },
   },
