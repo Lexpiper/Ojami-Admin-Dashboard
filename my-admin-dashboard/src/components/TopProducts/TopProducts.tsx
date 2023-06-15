5e08dfc13b96e89ba6a4e92ce39a6774702f29ac
Lexpiper <lexpiperfx@gmail.com>
2023-06-15 17:19:32 +0100

    add Products components

---
+++ my-admin-dashboard/src/components/TopProducts/TopProducts.tsx
@@ -0,0 +1,14 @@
+const TopProducts = () => {
+  return (
+    <div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae impedit
+        maxime blanditiis sit animi cum distinctio doloremque voluptate. Dolorem
+        minima vitae quo placeat eligendi repellendus explicabo ducimus ipsa id
+        libero.
+      </div>
+    </div>
+  );
+};
+
+export default TopProducts;
