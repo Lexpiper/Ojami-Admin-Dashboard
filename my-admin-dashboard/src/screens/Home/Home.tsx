3f6477090e4b29b9bb0fd3998d246b5e212bbb6d
Lexpiper <lexpiperfx@gmail.com>
2023-06-15 17:20:47 +0100

    add BarChart component to Home

--- my-admin-dashboard/src/screens/Home/Home.tsx
+++ my-admin-dashboard/src/screens/Home/Home.tsx
@@ -2,20 +2,49 @@ import Layout from "../../components/Layout";
 import Widgets from "../../components/Widgets/Widgets";
 import { widget } from "./data.js";
 import vector from "../../assets/Vector.png";
+import Barchart from "../../components/Chart/BarChart.js";
 
 const Home = () => {
+  const data = [
+    200000, 300000, 400000, 50000, 600000, 500000, 60000, 200000, 300000,
+    100000, 20000,
+  ];
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
   return (
     <Layout>
-      <div>
-        <div className="flex items-center pl-10">
+      <div className="mb-[100px]">
+        <div className="flex items-center ">
           <h3 className="pr-1">This month</h3>
           <img className="pt-1 text-xs cursor-pointer" src={vector} alt="" />
         </div>
-        <div className="grid  grid-cols-3 gap-20 px-10 pt-2 shadow-sm  ">
+        <div className="grid  grid-cols-3 gap-20  pb-10 pt-2 shadow-sm  ">
           {widget.map((i, index) => (
             <Widgets data={i} key={index} />
           ))}
         </div>
+        <div className="w-full h-full border-2 ">
+          <div className="flex items-center">
+
+          </div>
+          <Barchart data={data} labels={labels} />
+        </div>
+        {/* <div className="w-full h-full">
+          <TopProducts/>
+        </div> */}
       </div>
     </Layout>
   );
