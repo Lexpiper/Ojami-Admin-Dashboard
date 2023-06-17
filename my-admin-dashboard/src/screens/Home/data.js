c9023b9ba860ccfc5e80d719e2ebc55a12f05e1e
Lexpiper <lexpiperfx@gmail.com>
2023-06-17 19:55:39 +0100

    modify data to fix linking problem

--- my-admin-dashboard/src/screens/Home/data.js
+++ my-admin-dashboard/src/screens/Home/data.js
@@ -27,3 +27,26 @@ export const widget = [
     color: "text-[#1C1C1C]",
   },
 ];
+
+export const List = [
+  {
+    id: 1,
+    item: "All time",
+  },
+  {
+    id: 2,
+    item: "Today",
+  },
+  {
+    id: 3,
+    item: "This week",
+  },
+  {
+    id: 4,
+    item: "This month",
+  },
+  {
+    id: 5,
+    item: "Last 3 month",
+  },
+];
