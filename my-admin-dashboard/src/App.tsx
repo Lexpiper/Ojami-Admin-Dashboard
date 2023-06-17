c1f7adaa6eafd9321cccc588f6a20a3112f82af0
Lexpiper <lexpiperfx@gmail.com>
2023-06-17 20:01:46 +0100

    update app component to include sidebarcomponent with dynamic SVG color

--- my-admin-dashboard/src/App.tsx
+++ my-admin-dashboard/src/App.tsx
@@ -1,24 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./components/Login";
 import Home from "./screens/Home/Home";
 import Orders from "./screens/Orders/Orders";
 import Products from "./screens/Products/Products";
 
 function App() {
   return (
-    <div className="App dark:text-gray-100 dark:bg-slate-900 duration-100 overflow-hidden">
+    <div className="App duration-100 overflow-hidden">
       <BrowserRouter>
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
             <Route path="orders">
               <Route index element={<Orders />} />
             </Route>
-            {/* <Route path="products">
+            <Route path="products">
               <Route index element={<Products />} />
-            </Route> */}
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
