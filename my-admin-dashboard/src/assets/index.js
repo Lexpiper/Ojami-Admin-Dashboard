129281e03b3012520d1da5b182182e2b69467a0e
Lexpiper <lexpiperfx@gmail.com>
2023-06-17 20:07:45 +0100

    deleted files

--- my-admin-dashboard/src/assets/index.js
+++
@@ -1,6 +0,0 @@
-import dashIcon from "./dashIcon.png";
-import orderIcon from "./orderIcon.png";
-import productIcon from "./productIcon.png";
-import reportIcon from "./reportIcon.png";
-
-export { dashIcon, orderIcon, productIcon, reportIcon };
